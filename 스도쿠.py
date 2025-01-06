e900f6ca551081daaef0fe034e14ece5cb28aa13
Factories-git <official.factories@gmail.com>
2025-01-06 22:14:54 +0900

    BackTracking

--- 스도쿠.py
+++ 스도쿠.py
@@ -1,65 +1,39 @@
-import sys, time
-from collections import Counter
+import sys
 
 input = sys.stdin.readline
 
 
 def sudoku(x, y):
     global Board
-    nums = get_num(x, y)
-    for i in nums:
-        for j in range(len(nums)):
+    if x == 9:
+        return True
+    if y == 9:
+        return sudoku(x + 1, 0)
+    if Board[x][y] != 0:
+        return sudoku(x, y + 1)
+    for i in range(1, 10):
+        if check(x, y, i):
             Board[x][y] = i
-            if check(x, y):
-                return
+            if sudoku(x, y + 1):
+                return True
+            Board[x][y] = 0
+    return False
 
-def check(x, y):
-    width = []
-    length = []
-    range_ = []
+
+def check(x, y, n):
     for i in range(BOARD_WIDTH):
-        if Board[x][i] != 0:
-            width.append(Board[x][i])
-    for j in range(BOARD_LENGTH):
-        if Board[j][y] != 0:
-            length.append(Board[j][y])
-    start_x = (x // 3) * 3
-    start_y = (y // 3) * 3
-    for i in range(start_x, start_x + 3):
-        for j in range(start_y, start_y + 3):
-            if Board[i][j] != 0:
-                range_.append(Board[i][j])
-
-    w = Counter(width)
-    l = Counter(length)
-    r = Counter(range_)
-
-    if max(w.values()) > 1 or max(l.values()) > 1 or max(r.values()) > 1:
-        return False
-    return True
-
-
-def get_num(x, y):
-    empty_num = set(range(1, 10))
-    empty_num_1 = set(range(1, 10))
-    empty_num_2 = set(range(1, 10))
-
+        if Board[x][i] == n:
+            return False
     for i in range(BOARD_LENGTH):
-        if Board[x][i] != 0:
-            empty_num.discard(Board[x][i])
-    for i in range(BOARD_WIDTH):
-        if Board[i][y] != 0:
-            empty_num_1.discard(Board[i][y])
+        if Board[i][y] == n:
+            return False
     start_x = (x // 3) * 3
     start_y = (y // 3) * 3
     for i in range(start_x, start_x + 3):
         for j in range(start_y, start_y + 3):
-            if Board[i][j] != 0:
-                empty_num_2.discard(Board[i][j])
-    empty_num.intersection(empty_num_1)
-    empty_num.intersection(empty_num_2)
-    empty_num = list(empty_num)
-    return empty_num
+            if Board[i][j] == n:
+                return False
+    return True
 
 
 s = set(range(1, 9))
@@ -68,9 +42,6 @@ BOARD_LENGTH = 9
 Board = []
 for _ in range(BOARD_LENGTH):
     Board.append(list(map(int, input().split())))
-for x in range(BOARD_LENGTH):
-    for y in range(BOARD_WIDTH):
-        if Board[x][y] == 0:
-            sudoku(x, y)
+sudoku(0, 0)
 for row in Board:
     print(*row)
