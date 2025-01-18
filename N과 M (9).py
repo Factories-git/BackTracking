3dcab676c76c1aa60c4088a561b99d2e8d3db2e1
Factories-git <official.factories@gmail.com>
2025-01-18 21:10:19 +0900

    BackTracking

--- N과 M (9).py
+++ N과 M (9).py
@@ -1,23 +1,22 @@
 from collections import Counter
-
-
 n, m = map(int, input().split())
 arr = sorted(map(int, input().split()))
 ans = []
+c = [False] * (n+1)
 re = set()
-c = Counter(arr)
+
 def backtracking(depth):
     if depth == m:
         re.add(tuple(ans))
         return
 
     for i in range(n):
-        if arr[i] in ans and c[arr[i]] <= depth:
-            continue
-
-        ans.append(arr[i])
-        backtracking(depth + 1)
-        ans.pop()
+        if not c[i]:
+            c[i] = True
+            ans.append(arr[i])
+            backtracking(depth + 1)
+            ans.pop()
+            c[i] = False
 
 
 backtracking(0)
