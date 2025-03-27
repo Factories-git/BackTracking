f411e4952bb8fba4b4148b70c90c6eab1ea932c6
Factories-git <official.factories@gmail.com>
2025-03-28 08:36:05 +0900

    BackTracking

--- 비숍.py
+++ 비숍.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
 n = int(input())
 chess = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
@@ -5,22 +9,25 @@ diagonal = [False] * (2 * n - 1)
 re_diagonal = [False] * (2 * n - 1)
 
 
-def solve(y, c):
-    global ans
+def is_safe(x, y):
+    return not diagonal[x + y] and not re_diagonal[x - y]
 
+
+def solve(y, x, c):
+    global ans
     if y == n:
         ans = max(ans, c)
         return
+    if x == n:
+        solve(y + 1, 0, c)
+    if chess[x][y] == 0 and not is_safe(x, y):
+        return
+    for i in range(x, n):
+        diagonal[y + i] = re_diagonal[y - i] = True
+        solve(y, i, c + 1)
+        print(y, i, c, x)
+        diagonal[y + i] = re_diagonal[y - i] = False
 
-    def is_safe(x, y):
-        return not diagonal[x + y] and not re_diagonal[x - y]
 
-    for i in range(n):
-        if is_safe(y, i):
-            diagonal[y + i] = re_diagonal[y - i] = True
-            solve(y + 1, c + 1)
-            diagonal[y + i] = re_diagonal[y - i] = False
-
-
-solve(0, 0)
+solve(0, 0, 0)
 print(ans)
